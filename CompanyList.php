026b21e38d2577c37207e8113e05202e5e3bf071
Tyler Robbins <tyler@familyrobbins.com>
2015-08-08 04:45:15 +0000

    Added comments. Fixed line ending.

--- CompanyList.php
+++ CompanyList.php
@@ -1,4 +1,10 @@
 <?php
+  /*
+  * CompanyList.php
+  * Lists all of the companies in the database, and links to a list of all IP addresses associated with that company.
+  */
+
+  // Get all info on how to access the database
   include 'Config.php';
   $cfg = new Config("/home/tyler/Desktop/dev/Ping-Monitor/pingMonitor.cfg");
   $user = $cfg->getOption("DB_USER");
@@ -16,6 +22,7 @@
 	die('Error when opening database:\n\t' . mysqli_error());
   }
 
+  // Exactly what the name implies
   function readCompanyList(){
 	global $link;
 	$sql_query = "SELECT * FROM CompanyList";
@@ -24,16 +31,24 @@
 	if(!$result){
 		die('Error when querying database:<p>' . mysqli_error());
 	}
+
+   	// Because mysqli_blah functions are really stupid in how they return data,
+  	// We need to place all of it manually into an indexed array (index:value, not key:value!)
 	for($idx = 0; $idx < mysqli_num_rows($result); $idx++){
 		array_push($rows,mysqli_fetch_array($result,MYSQL_NUM));
 	}
 	return $rows;
   }
 
+  // Now that that is all done, read the comapnies...
   $result = readCompanyList();
+
+  // And print them out
   for($edx = 0; $edx < sizeof($result);$edx++){
 	  echo "<p>";
 	  $cName = $result[$edx][1];
+	  // Format the output so that it links to iplist.php with the appropriate CompanyID
 	  echo "<A href=\"iplist.php?id=" . $result[$edx][0] . "\">" . $cName . "</A>";
   }
 ?>
+
